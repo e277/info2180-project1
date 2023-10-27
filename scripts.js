a6dae478d76b551a547aed3d52d7d60390407c20
e277 <ezramuir12@gmail.com>
2023-10-27 11:27:47 -0500

    Revert "Finished email implementation"
    This reverts commit df9404c1f367f69d41dd9500c8dd1abc01641837.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,1 @@
 /* Add your JavaScript to this file */
-window.onload = function() {
-    const email = document.getElementById("email");
-    const message = document.getElementsByClassName("message")[0];
-    const button = document.getElementsByClassName("btn")[1];
-
-    button.addEventListener("click", (event) => {
-        event.preventDefault();
-
-        if (email.value === "") {
-            message.innerHTML = "Please enter a valid email address";
-        } else {
-            message.innerHTML = `Thank you! Your email address ${email.value} has been added to our mailing list!`;
-        }
-    })
-    
-}
